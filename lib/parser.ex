556d28c138b51ff0e85ab9a028e70348d89a2c94
Marko Vukovic <mvkvc.git@gmail.com>
2023-04-26 00:44:19 -0400

    Implement parser

--- lib/parser.ex
+++ lib/parser.ex
@@ -38,5 +38,40 @@ defmodule Parser do
   Parses the raw consumption data in consumption_data to internal data format
   """
   def parse(consumption_data) do
+    data =
+      consumption_data
+      |> Map.get("result")
+      |> Enum.at(0)
+      |> Map.get("MyEnergyData_MarketDocument")
+      |> Map.get("TimeSeries")
+      |> Enum.at(0)
+
+    id = Map.get(data, "metering_point_id")
+    periods = Map.get(data, "Period")
+
+    Enum.reduce(periods, [], fn x, acc ->
+      parse_period(x, id) ++ acc
+    end)
+  end
+
+  defp parse_period(period, id) do
+    time_start = get_in(period, ["timeInterval", "start"])
+    time_end = get_in(period, ["timeInterval", "end"])
+    point = Map.get(period, "Point")
+
+    Enum.reduce(point, [], fn x, acc ->
+      [parse_point(x, id, time_start, time_end)] ++ acc
+    end)
+  end
+
+  defp parse_point(point, id, time_start, time_end) do
+    quantity = Map.get(point, "out_Quantity.quantity")
+
+    %{
+      metering_point_id: id,
+      quantity: quantity,
+      interval_start: time_start,
+      interval_end: time_end
+    }
   end
 end
